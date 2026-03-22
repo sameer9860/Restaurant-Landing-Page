b4eab8fc1205db85422055124743c66a345e901e
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-22 17:44:05 +0545

    feat:testimonial images

--- src/constants/restaurantConstants.js
+++ src/constants/restaurantConstants.js
@@ -22,3 +22,91 @@ export const COLORS = {
   SECONDARY: "#8b5a00",
 };
 
+export const IMAGES = {
+  HERO: "https://lh3.googleusercontent.com/aida-public/AB6AXuDai4g7ZilfiQ9dH5ohVThhXt_y1vAY2h4FabkY8DLhv0EXjYam08wAMWmVuRtHVKkK5k6EgGz8apQEdyEfmPKsfor1ImDtpVsywMJLCVfNhMPFjoPxmVRf1I-_ISkE7S3zdPar5k65uYzPkOXE6KVQkoqRflP42Z0b--GqJUXqYi3KIP3c0zGxkpo_xnswPjZQJ1akrUMhxJ_WqIXNjbTfGLz-iSSG4FmCNnJvRVPD-lXUQz41sJkyEB7EYQnaiVXaS5CH0ZRhdjLf",
+  ABOUT: "https://lh3.googleusercontent.com/aida-public/AB6AXuCChgvnep9I-Zq0qma3NloeSOT2IRDal6PA1BUULNhrFRDvZ9blqE2_tvp3ewXVCcWvnHzYImgCzUH4qZwh9kmc32P7h2370kk_fGQaNPfpb_0WYSrQQHIXobUv_uwsIKeYxv0GAbKCmgzU3EF1ReKYw-JS10mJGzlgdyDv14508Y0rqoh46txQqpaejndWWYXIGpb1nh_X9X-GMRjUMEGnYKpF9hTeO5Rzi2CHcINFdqqVedPLJZh0iYBFe5X-TNNdTx7ZeNcTWols",
+};
+
+export const MENU_HIGHLIGHTS = [
+  {
+    id: "pizza",
+    title: "Signature Pizzas",
+    description: "Margherita with fresh basil & Chicken BBQ with smoked mozzarella.",
+    price: "From Rs. 450",
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBArHKUIpJOGfqagc-3BOFQV9OTJ7HVnYL9daClV0f6vZEVmxFgOYu5o6j9-ooQCL7AvEl523216H5h5PIo1RYUh-SaWy4opMQ6T0sUzshBr1XDLB9NHV69vf9vPsQ5YW6jJoNDLBADrkdGKIMS47KP_HWRGoFdW3cXnMzmip4hyBzQEiyDrpQIhTKH2JhhJB7C66AwYjM1WXuAqmAlq7EmIPjYQ6jL0-_cu3Oy3itLhrmXFPhRN31Qshhy2ZZHIvef7k2GlqbYmntP",
+    colSpan: "md:col-span-8",
+    height: "h-[400px]",
+  },
+  {
+    id: "burger",
+    title: "Iconic Burgers",
+    description: "Classic Beef & Garden Veggie",
+    price: "From Rs. 550",
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuARE-RzjSwyBnuWcjzllrMk7ch45AwO116lRj-1iJNn-TmmhqEIaQ7YgPk3WCNOM_SZGSKpN6NX1xK9uKXL8k7oOPEitc4Giqb9bl3rNMy7GhqE-sBFGN2pYYdXE2EHShen_jCLzDtULGBRAFEFLAhPAfZnACDewf6anciTylAdeQD3TvSGykb8rcpxuNNwGA0ozFpXqEKeTLlthYyZEXdBahoPg5As1Bh80tCeO7C7sQ4YEjtst-LChTg8VIiJJImjanpi2cYTUb4W",
+    colSpan: "md:col-span-4",
+    height: "h-[400px]",
+    textCenter: true,
+  },
+  {
+    id: "pasta",
+    title: "Authentic Pasta",
+    description: "Carbonara & Veggie Penne",
+    price: "From Rs. 650",
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBXE6iwvwc3Tv_IuitHBH63Em0gkv_fZXN_a37_opzYAkEB2OE7DF_yKp6ypvsLAOogsAEYethB5GKBJfJL-Y3boULWht4_rnvRdLeQSxmxisH3hfpfLmPYC31NhmJVwdtErqGkrsvYyICmzI3mVuT5LJTjao8Ux77omszoS-i8hPubKZXZ4TLhNfLxQ6LjN53vTTvk2fw9WGLUWc9iZ0kpZntj0bkV6ZaVUcLZWTrkcKPKFQHJMuKYDQeh-zpAu3HHcGUu5siUrZqA",
+    colSpan: "md:col-span-4",
+    height: "h-[350px]",
+    textCenter: true,
+  },
+];
+
+export const OFFERS = [
+  {
+    title: "Combo Meal",
+    description: "Pizza + Pasta + Soft Drink",
+    discount: "20% Off",
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBQYHohMDw_Hqu6x9onCRtXzEjWczOIsSPrpDDjqRyb-4UfCeQK7cD88ARNXsnvgER_l6mH2uO342eIb20fEZ5u5WUsovdhfyszKIz_MrBUptsCQibNhXSKZ2Nby12FXoIzgIs0HC69Jme9hX7B4z958CE1rJczzjhg_Vj0KBjDPg1q7kvlIh5_ogjjdUuzUQEqOLlwncTmrd76AQPenTTKsdW88pDTt4294rEihyf3E5Hhuge15uSoe6Z4AGZt8o9obbXarFA5sRew",
+    label: "Bestseller",
+    labelColor: "bg-primary",
+    discountColor: "text-primary",
+  },
+  {
+    title: "Burger Meal",
+    description: "Any Burger + Fries + Drink",
+    discount: "Free Dessert",
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBrRpUOniyzpQmIkfLtN3SG8tRG9lqRyKE2LHwP84RQ6aZ-ZoU4BBNkRKLgdg6_Sdu81Cl3oDqBeQdUHzwe8Jx0fqbYlYmztVhIx7jZ3yCHqXkqAbsDU0fJPsgjLiJs6K1NTBdGXBABqLlrdy8MeXySyIEM4GhkiBtS9OH9Gci0SJaVzXcIQtTJBPaxk6qdcdPbQfS5TlUlN_vOk9leGQnxbLJcj0CWjwd3UkEmKR-JDJ5dyTwZUj0nSK4Nhr9ZjWpBU7jGlyp50JLs",
+    label: "New",
+    labelColor: "bg-secondary",
+    discountColor: "text-secondary",
+  },
+];
+
+export const TESTIMONIALS = [
+  { 
+    name: "Sita R.", 
+    initials: "SR", 
+    text: "Best pizza in Nepal! The crust is so light and the toppings are generous. Truly authentic taste.",
+    bgColor: "bg-primary-container",
+    textColor: "text-on-primary-container"
+  },
+  { 
+    name: "Raj P.", 
+    initials: "RP", 
+    text: "Amazing pasta and friendly staff. The Spaghetti Carbonara is to die for. Definitely coming back!",
+    bgColor: "bg-secondary-container",
+    textColor: "text-on-secondary-container"
+  },
+  { 
+    name: "Mina T.", 
+    initials: "MT", 
+    text: "Love their burgers! They are juicy, flavorful, and the fries are perfectly crispy. A must-try.",
+    bgColor: "bg-tertiary-container",
+    textColor: "text-on-tertiary-container"
+  },
+];
+
+export const SOCIAL_LINKS = [
+  { icon: "social_leaderboard", label: "facebook", href: "#" },
+  { icon: "photo_camera", label: "instagram", href: "#" },
+  { icon: "movie", label: "tiktok", href: "#" },
+];
+
